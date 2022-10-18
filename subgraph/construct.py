cf46a188655e2817cfc393eb58b05bc346e7e1f6
Chendi <1049653012@qq.com>
2022-10-19 03:11:59 +0800

    construct subgraphs for random sampling

--- subgraph/construct.py
+++ subgraph/construct.py
@@ -2,7 +2,57 @@ from typing import List
 
 import torch
 from torch_geometric.data import Data, Batch
+from torch_geometric.utils import subgraph
 from subgraph.grad_utils import Nodemask2Edgemask, CenterNodeIdentityMapping, nodemask2edgemask, centralize
+
+
+# Todo: check if NGNN has delete nodes option
+def construct_random_local_structure_subgraphs(graphs: List[Data],
+                                               node_mask: torch.FloatTensor,
+                                               nnodes_wo_duplicate: torch.LongTensor,
+                                               batch_wo_duplicate: torch.LongTensor,
+                                               y_wo_duplicate: torch.Tensor,
+                                               subgraph2node_aggr: str, ):
+    """
+
+    :param graphs:
+    :param node_mask:
+    :param nnodes_wo_duplicate:
+    :param batch_wo_duplicate:
+    :param y_wo_duplicate:
+    :param subgraph2node_aggr:
+    :return:
+    """
+    subgraphs = []
+    for i, g in enumerate(graphs):
+        subgraphs += [g] * g.nnodes
+
+    new_batch = Batch.from_data_list(subgraphs)
+    new_edge_index, new_edge_attr = subgraph(node_mask,
+                                             new_batch.edge_index,
+                                             new_batch.edge_attr,
+                                             relabel_nodes=True,
+                                             num_nodes=new_batch.num_nodes)
+
+    new_data = Data(x=new_batch.x[node_mask],
+                    edge_index=new_edge_index,
+                    edge_attr=new_edge_attr,
+                    edge_weights=None,
+                    y=y_wo_duplicate,)
+    new_data.subgraphs2nodes = new_batch.batch[node_mask]
+    new_data.batch = batch_wo_duplicate
+    new_data.num_graphs = len(subgraphs)
+
+    assert subgraph2node_aggr in ['add', 'center']
+    if subgraph2node_aggr == 'add':
+        new_data.node_mask = torch.ones(node_mask.sum(), dtype=torch.bool, device=node_mask.device)
+    elif subgraph2node_aggr == 'center':
+        nnodes = torch.repeat_interleave(nnodes_wo_duplicate, nnodes_wo_duplicate, dim=0)
+        new_data.node_mask = centralize(node_mask, nnodes_wo_duplicate, nnodes)
+        new_data.node_mask = new_data.node_mask[node_mask]
+        new_data.subgraphs2nodes = new_data.subgraphs2nodes[new_data.node_mask]
+
+    return new_data
 
 
 def construct_imle_local_structure_subgraphs(graphs: List[Data],
