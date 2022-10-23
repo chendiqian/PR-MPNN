0a63b9d887f27dfde7da0fbe002b6fcf37afdcc3
Chendi <1049653012@qq.com>
2022-10-23 16:53:19 +0800

    add env installation bash

---
+++ env_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo y | conda create -n diffstruct python=3.8
+conda activate diffstruct
+
+pip install ml-collections
+pip install numba
+pip install tqdm
+pip install ortools
+pip install sacred
+pip install PyYAML
+pip install ogb
+
+conda install pytorch==1.10.1 cudatoolkit=11.3 -c pytorch -c conda-forge
+pip install https://data.pyg.org/whl/torch-1.10.0%2Bcu113/torch_scatter-2.0.9-cp38-cp38-linux_x86_64.whl
+pip install https://data.pyg.org/whl/torch-1.10.0%2Bcu113/torch_sparse-0.6.13-cp38-cp38-linux_x86_64.whl
+pip install torch-geometric
+
+pip install tensorboard
