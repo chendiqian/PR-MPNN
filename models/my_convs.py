8c6ef4c4e9532860495b57baed7ac40c79a1a8ba
Chendi Qian <1049653012@qq.com>
2022-11-13 21:18:23 +0800

    refactor

---
+++ models/my_convs.py
@@ -0,0 +1,47 @@
+import torch
+from torch_geometric.nn import MessagePassing
+
+from models.encoder import BondEncoder
+
+
+class GINConv(MessagePassing):
+    def __init__(self, emb_dim):
+        """
+            emb_dim (int): node embedding dimensionality
+        """
+
+        super(GINConv, self).__init__(aggr="add")
+
+        self.mlp = torch.nn.Sequential(torch.nn.Linear(emb_dim, 2 * emb_dim),
+                                       torch.nn.BatchNorm1d(2 * emb_dim),
+                                       torch.nn.ReLU(),
+                                       torch.nn.Linear(2 * emb_dim, emb_dim))
+        self.eps = torch.nn.Parameter(torch.Tensor([0.]))
+
+        self.bond_encoder = BondEncoder(emb_dim=emb_dim)
+
+    def forward(self, x, edge_index, edge_attr, edge_weight):
+        if edge_weight is not None and edge_weight.ndim < 2:
+            edge_weight = edge_weight[:, None]
+
+        edge_embedding = self.bond_encoder(edge_attr)
+        out = self.mlp((1 + self.eps) * x + self.propagate(edge_index,
+                                                           x=x,
+                                                           edge_attr=edge_embedding,
+                                                           edge_weight=edge_weight))
+        return out
+
+    def message(self, x_j, edge_attr, edge_weight):
+        m = torch.relu(x_j + edge_attr)
+        return m * edge_weight if edge_weight is not None else m
+
+    def update(self, aggr_out):
+        return aggr_out
+
+    def reset_parameters(self):
+        lst = torch.nn.ModuleList(self.mlp)
+        for l in lst:
+            if not isinstance(l, torch.nn.ReLU):
+                l.reset_parameters()
+        self.eps = torch.nn.Parameter(torch.Tensor([0.]).to(self.eps.device))
+        self.bond_encoder.reset_parameters()
