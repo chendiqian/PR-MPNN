4ef4ba073b4a7f353a45eacbd5b9c8dea9071b44
Chendi Qian <1049653012@qq.com>
2022-11-19 15:58:25 +0800

    rename, add gin wo edge

--- models/my_convs.py
+++ models/my_convs.py
@@ -8,11 +8,48 @@ from .nn_utils import reset_sequential_parameters, MLP
 
 
 class GINConv(MessagePassing):
+    def __init__(self, emb_dim: int = 64, mlp: Optional[Union[MLP, torch.nn.Sequential]] = None,):
+        super(GINConv, self).__init__(aggr="add")
+
+        if mlp is not None:
+            self.mlp = mlp
+        else:
+            self.mlp = MLP([emb_dim, emb_dim, emb_dim], norm=True, dropout=0.)
+
+        self.eps = torch.nn.Parameter(torch.Tensor([0.]))
+
+    def forward(self, x, edge_index, edge_weight):
+        if edge_weight is not None and edge_weight.ndim < 2:
+            edge_weight = edge_weight[:, None]
+
+        out = self.mlp((1 + self.eps) * x + self.propagate(edge_index,
+                                                           x=x,
+                                                           edge_weight=edge_weight))
+        return out
+
+    def message(self, x_j, edge_weight):
+        m = torch.relu(x_j)
+        return m * edge_weight if edge_weight is not None else m
+
+    def update(self, aggr_out):
+        return aggr_out
+
+    def reset_parameters(self):
+        self.eps = torch.nn.Parameter(torch.Tensor([0.]).to(self.eps.device))
+        if isinstance(self.mlp, (MLP, torch.nn.Linear)):
+            self.mlp.reset_parameters()
+        elif isinstance(self.mlp, torch.nn.Sequential):
+            reset_sequential_parameters(self.mlp)
+        else:
+            raise TypeError
+
+
+class GINEConv(MessagePassing):
     def __init__(self, emb_dim: int = 64,
                  mlp: Optional[Union[MLP, torch.nn.Sequential]] = None,
                  bond_encoder: Optional[Union[MLP, torch.nn.Sequential]] = None):
 
-        super(GINConv, self).__init__(aggr="add")
+        super(GINEConv, self).__init__(aggr="add")
 
         if mlp is not None:
             self.mlp = mlp
