1cf7bc4a038e49655965b0ab76069bdc1cc4b229
Chendi Qian <1049653012@qq.com>
2022-11-19 20:25:18 +0800

    add gcn

--- models/my_convs.py
+++ models/my_convs.py
@@ -2,9 +2,42 @@ from typing import Optional, Union
 
 import torch
 from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import degree
+from torch_geometric.utils import add_remaining_self_loops
 
 from .encoder import BondEncoder
 from .nn_utils import reset_sequential_parameters, MLP
+
+
+class GCNConv(MessagePassing):
+
+    def __init__(self, in_channels: int, out_channels: int, add_self_loops: bool = True):
+
+        super(GCNConv, self).__init__(aggr='add')
+
+        self.add_self_loops = add_self_loops
+        self.lin = torch.nn.Linear(in_channels, out_channels)
+
+    def reset_parameters(self):
+        self.lin.reset_parameters()
+
+    def forward(self, x, edge_index, edge_weight):
+        """"""
+        if self.add_self_loops:
+            edge_index, _ = add_remaining_self_loops(edge_index, None, 1., x.shape[0])
+
+        row, col = edge_index
+        deg = degree(row, x.shape[0], dtype=torch.float)
+        deg_inv_sqrt = deg.pow(-0.5)
+        deg_inv_sqrt[deg_inv_sqrt == float('inf')] = 0
+        norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
+
+        x = self.lin(x)
+        return self.propagate(edge_index, x=x, norm=norm, edge_weight=edge_weight)
+
+    def message(self, x_j, norm, edge_weight):
+        m = norm.view(-1, 1) * x_j
+        return m * edge_weight if edge_weight is not None else m
 
 
 class GINConv(MessagePassing):
