0e49ae6e57e8360d2a307769e3f2c2dcbc7b1fc9
Chendi Qian <1049653012@qq.com>
2022-11-23 11:05:07 +0800

    add arxiv sage

---
+++ models/downstream_models/arxiv_sage.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn.functional as F
+
+from models.my_convs import SAGEConv
+
+
+class ArxivSAGE(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout):
+        super(ArxivSAGE, self).__init__()
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(SAGEConv(in_channels, hidden_channels))
+        self.bns = torch.nn.ModuleList()
+        self.bns.append(torch.nn.BatchNorm1d(hidden_channels))
+        for _ in range(num_layers - 2):
+            self.convs.append(SAGEConv(hidden_channels, hidden_channels))
+            self.bns.append(torch.nn.BatchNorm1d(hidden_channels))
+        self.convs.append(SAGEConv(hidden_channels, out_channels))
+
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.bns:
+            bn.reset_parameters()
+
+    def forward(self, data):
+        x, edge_index, edge_weight = data.x, data.edge_index, data.edge_weight
+        for i, conv in enumerate(self.convs[:-1]):
+            x = conv(x, edge_index, edge_weight)
+            x = self.bns[i](x)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, edge_index, edge_weight)
+        return x
