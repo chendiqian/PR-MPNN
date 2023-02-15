5a123e19d0b03af85a6805d59cf237101fcd1b4f
Chendi Qian <1049653012@qq.com>
2023-02-15 17:49:23 +0100

    log noise scale

--- main.py
+++ main.py
@@ -44,6 +44,7 @@ def naming(args) -> str:
         name += f'H{args.imle_configs.emb_hid_size}'
         name += f'L_{args.imle_configs.gnn_layer}_{args.imle_configs.mlp_layer}'
         name += f'DP{args.imle_configs.dropout}'
+        name += f'noise{args.imle_configs.noise_scale}_'
         name += f'Beta{args.imle_configs.beta}_'
 
         name += 'encoding_'
