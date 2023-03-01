be42c578fd0ae7545462d8e3d83a9097be7a9f12
Chendi Qian <1049653012@qq.com>
2023-03-01 18:01:53 +0100

    fix bug

--- training/imle_scheme.py
+++ training/imle_scheme.py
@@ -99,6 +99,10 @@ class IMLEScheme:
         #             sample_instance_idx.append((logit >= thresh[None]).to(torch.float))
         if self.imle_sample_policy == 'graph_topk':
             # local_logits: (Batch, Nmax, Nmax, ensemble)
+            Nmax = local_logits.shape[1]
+            if self.sample_k >= Nmax:
+                return local_logits.new_zeros(local_logits.shape)
+
             local_logits[~self.real_node_node_mask] -= 1.e10
             thresh = torch.topk(local_logits, self.sample_k, dim=2, largest=True, sorted=True).values[:, :, -1, :][:, :, None, :]
             mask = (local_logits >= thresh).to(torch.float)
