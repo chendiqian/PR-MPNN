e14e85a3431b8c5ff24a347010176ba93e1d79c9
Chendi Qian <1049653012@qq.com>
2023-03-17 16:50:48 +0100

    use torch 1.13

--- env_install.sh
+++ env_install.sh
@@ -12,9 +12,10 @@ pip install sacred
 pip install PyYAML
 pip install setuptools==59.5.0
 
-echo y | conda install pytorch==1.10.1 cudatoolkit=11.3 -c pytorch -c conda-forge
-pip install https://data.pyg.org/whl/torch-1.10.0%2Bcu113/torch_scatter-2.0.9-cp38-cp38-linux_x86_64.whl
-pip install https://data.pyg.org/whl/torch-1.10.0%2Bcu113/torch_sparse-0.6.13-cp38-cp38-linux_x86_64.whl
+conda install -y pytorch=1.13.1 pytorch-cuda=11.7 -c pytorch -c nvidia
+pip install https://data.pyg.org/whl/torch-1.13.0%2Bcu116/pyg_lib-0.1.0%2Bpt113cu116-cp38-cp38-linux_x86_64.whl
+pip install https://data.pyg.org/whl/torch-1.13.0%2Bcu116/torch_scatter-2.1.1%2Bpt113cu116-cp38-cp38-linux_x86_64.whl
+pip install https://data.pyg.org/whl/torch-1.13.0%2Bcu116/torch_sparse-0.6.17%2Bpt113cu116-cp38-cp38-linux_x86_64.whl
 pip install torch-geometric
 
 pip install tensorboard
