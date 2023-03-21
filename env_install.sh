6e0adc25efb455e34974725c997815a142caf6c9
Chendi Qian <1049653012@qq.com>
2023-03-21 11:06:53 +0100

    renew envs

--- env_install.sh
+++ env_install.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-echo y | conda create -n diffstruct python=3.8
+echo y | conda create -n diffstruct python=3.10
 #conda activate diffstruct
 source ${CONDA_PREFIX}/bin/activate diffstruct
 
 pip install ml-collections
-pip install numba
-pip install tqdm
-pip install ortools
+#pip install numba
 pip install sacred
 pip install PyYAML
-pip install setuptools==59.5.0
+pip install wandb
 
-conda install -y pytorch=1.13.1 pytorch-cuda=11.6 -c pytorch -c nvidia
-pip install https://data.pyg.org/whl/torch-1.13.0%2Bcu116/pyg_lib-0.1.0%2Bpt113cu116-cp38-cp38-linux_x86_64.whl
-pip install https://data.pyg.org/whl/torch-1.13.0%2Bcu116/torch_scatter-2.1.1%2Bpt113cu116-cp38-cp38-linux_x86_64.whl
-pip install https://data.pyg.org/whl/torch-1.13.0%2Bcu116/torch_sparse-0.6.17%2Bpt113cu116-cp38-cp38-linux_x86_64.whl
+conda install -y pytorch=2.0.0 pytorch-cuda=11.8 -c pytorch -c nvidia
+pip install git+https://github.com/pyg-team/pyg-lib.git
+pip install git+https://github.com/rusty1s/pytorch_scatter.git
+pip install https://data.pyg.org/whl/torch-2.0.0%2Bcu118/torch_sparse-0.6.17%2Bpt20cu118-cp310-cp310-linux_x86_64.whl
 pip install torch-geometric
 
 pip install ogb
