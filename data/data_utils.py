cb75b53b8222deaee1493bd2c3551f99e0d71768
Chendi Qian <1049653012@qq.com>
2023-03-21 11:06:43 +0100

    temporary comment unused

--- data/data_utils.py
+++ data/data_utils.py
@@ -2,32 +2,32 @@ from collections import namedtuple, deque
 from typing import List, Union, Tuple, Optional
 import time
 
-import numba
-import numpy as np
+# import numba
+# import numpy as np
 import torch
-from torch import Tensor
-from torch import device as TorchDevice
+# from torch import Tensor
+# from torch import device as TorchDevice
 
 
-class SubgraphSetBatch:
-    def __init__(self, **kwargs):
-        for k, v in kwargs.items():
-            setattr(self, k, v)
-
-    def to(self, device: TorchDevice):
-        for k, v in self.__dict__.items():
-            if isinstance(v, Tensor):
-                setattr(self, k, v.to(device))
-        return self
-
-    def __repr__(self):
-        string = []
-        for k, v in self.__dict__.items():
-            if isinstance(v, Tensor):
-                string.append(k + f': Tensor {list(v.shape)}')
-            else:
-                string.append(k + ': ' + type(v).__name__)
-        return ' '.join(string)
+# class SubgraphSetBatch:
+#     def __init__(self, **kwargs):
+#         for k, v in kwargs.items():
+#             setattr(self, k, v)
+#
+#     def to(self, device: TorchDevice):
+#         for k, v in self.__dict__.items():
+#             if isinstance(v, Tensor):
+#                 setattr(self, k, v.to(device))
+#         return self
+#
+#     def __repr__(self):
+#         string = []
+#         for k, v in self.__dict__.items():
+#             if isinstance(v, Tensor):
+#                 string.append(k + f': Tensor {list(v.shape)}')
+#             else:
+#                 string.append(k + ': ' + type(v).__name__)
+#         return ' '.join(string)
 
 
 AttributedDataLoader = namedtuple(
@@ -38,64 +38,64 @@ AttributedDataLoader = namedtuple(
     ])
 
 
-@numba.njit(cache=True)
-def edgeindex2neighbordict(edge_index: np.ndarray, num_nodes: int) -> List[np.ndarray]:
-    """
-
-    :param edge_index: shape (2, E)
-    :param num_nodes:
-    :return:
-    """
-    neighbors = [[-1] for _ in range(num_nodes)]
-    for i, node in enumerate(edge_index[0]):
-        neighbors[node].append(edge_index[1][i])
-
-    for i, n in enumerate(neighbors):
-        n.pop(0)
-
-    neighbors = [np.array(n, dtype=np.int64) for n in neighbors]
-    return neighbors
-
-
-def get_ptr(graph_idx: Tensor, device: torch.device = None) -> Tensor:
-    """
-    Given indices of graph, return ptr
-    e.g. [1,1,2,2,2,3,3,4] -> [0, 2, 5, 7]
-
-    :param graph_idx:
-    :param device:
-    :return:
-    """
-    if device is None:
-        device = graph_idx.device
-    return torch.cat((torch.tensor([0], device=device),
-                      (graph_idx[1:] > graph_idx[:-1]).nonzero().reshape(-1) + 1,
-                      torch.tensor([len(graph_idx)], device=device)), dim=0)
-
-
-def get_connected_components(subset, neighbor_dict):
-    components = []
-
-    while subset:
-        cur_node = subset.pop()
-        q = deque()
-        q.append(cur_node)
-
-        component = [cur_node]
-        while q:
-            cur_node = q.popleft()
-            i = 0
-            while i < len(subset):
-                candidate = subset[i]
-                if candidate in neighbor_dict[cur_node]:
-                    subset.pop(i)
-                    component.append(candidate)
-                    q.append(candidate)
-                else:
-                    i += 1
-        components.append(component)
-
-    return components
+# @numba.njit(cache=True)
+# def edgeindex2neighbordict(edge_index: np.ndarray, num_nodes: int) -> List[np.ndarray]:
+#     """
+#
+#     :param edge_index: shape (2, E)
+#     :param num_nodes:
+#     :return:
+#     """
+#     neighbors = [[-1] for _ in range(num_nodes)]
+#     for i, node in enumerate(edge_index[0]):
+#         neighbors[node].append(edge_index[1][i])
+#
+#     for i, n in enumerate(neighbors):
+#         n.pop(0)
+#
+#     neighbors = [np.array(n, dtype=np.int64) for n in neighbors]
+#     return neighbors
+#
+#
+# def get_ptr(graph_idx: Tensor, device: torch.device = None) -> Tensor:
+#     """
+#     Given indices of graph, return ptr
+#     e.g. [1,1,2,2,2,3,3,4] -> [0, 2, 5, 7]
+#
+#     :param graph_idx:
+#     :param device:
+#     :return:
+#     """
+#     if device is None:
+#         device = graph_idx.device
+#     return torch.cat((torch.tensor([0], device=device),
+#                       (graph_idx[1:] > graph_idx[:-1]).nonzero().reshape(-1) + 1,
+#                       torch.tensor([len(graph_idx)], device=device)), dim=0)
+#
+#
+# def get_connected_components(subset, neighbor_dict):
+#     components = []
+#
+#     while subset:
+#         cur_node = subset.pop()
+#         q = deque()
+#         q.append(cur_node)
+#
+#         component = [cur_node]
+#         while q:
+#             cur_node = q.popleft()
+#             i = 0
+#             while i < len(subset):
+#                 candidate = subset[i]
+#                 if candidate in neighbor_dict[cur_node]:
+#                     subset.pop(i)
+#                     component.append(candidate)
+#                     q.append(candidate)
+#                 else:
+#                     i += 1
+#         components.append(component)
+#
+#     return components
 
 
 def scale_grad(model: torch.nn.Module, scalar: Union[int, float]) -> torch.nn.Module:
@@ -162,24 +162,24 @@ class SyncMeanTimer:
             return self.last_end_time
 
 
-def edge_index2dense_adj(edge_index: Tensor,
-                         num_nodes: Optional[int] = None,
-                         max_nodes: Optional[int] = None,
-                         default_dtype: torch.dtype = torch.int):
-    """
-
-    @param edge_index:
-    @param num_nodes:
-    @param max_nodes:
-    @param default_dtype:
-    @return:
-    """
-    if num_nodes is None:
-        num_nodes = edge_index.max()
-
-    if max_nodes is None:
-        max_nodes = num_nodes
-
-    adj = torch.zeros(num_nodes, max_nodes, device=edge_index.device, dtype=default_dtype)
-    adj[edge_index[0], edge_index[1]] = 1
-    return adj
+# def edge_index2dense_adj(edge_index: Tensor,
+#                          num_nodes: Optional[int] = None,
+#                          max_nodes: Optional[int] = None,
+#                          default_dtype: torch.dtype = torch.int):
+#     """
+#
+#     @param edge_index:
+#     @param num_nodes:
+#     @param max_nodes:
+#     @param default_dtype:
+#     @return:
+#     """
+#     if num_nodes is None:
+#         num_nodes = edge_index.max()
+#
+#     if max_nodes is None:
+#         max_nodes = num_nodes
+#
+#     adj = torch.zeros(num_nodes, max_nodes, device=edge_index.device, dtype=default_dtype)
+#     adj[edge_index[0], edge_index[1]] = 1
+#     return adj
