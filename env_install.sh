b78ed5c3d5045097aa80d230cbb1a0dfc8e66038
Chendi Qian <1049653012@qq.com>
2023-03-24 11:15:13 +0100

    for nightly 2.1.0

--- env_install.sh
+++ env_install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-echo y | conda create -n diffstruct python=3.10
+echo y | conda create -n nightly python=3.10
 #conda activate diffstruct
-source ${CONDA_PREFIX}/bin/activate diffstruct
+source ${CONDA_PREFIX}/bin/activate nightly
 
+conda install pytorch  pytorch-cuda=11.7 -c pytorch-nightly -c nvidia
+pip install cmake
+pip install --verbose git+https://github.com/pyg-team/pyg-lib.git
+pip install --verbose torch_scatter
+pip install --verbose torch_sparse
+pip install --verbose torch_geometric
+
+pip install ogb
 pip install ml-collections
 #pip install numba
 pip install sacred
 pip install PyYAML
 pip install wandb
 pip install matplotlib
-
-conda install -y pytorch=2.0.0 pytorch-cuda=11.8 -c pytorch -c nvidia
-pip install https://data.pyg.org/whl/torch-2.0.0%2Bcu118/torch_scatter-2.1.1%2Bpt20cu118-cp310-cp310-linux_x86_64.whl
-pip install https://data.pyg.org/whl/torch-2.0.0%2Bcu118/torch_sparse-0.6.17%2Bpt20cu118-cp310-cp310-linux_x86_64.whl
-conda install -y pyg -c pyg
-
-pip install ogb
