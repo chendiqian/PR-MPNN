1971e5b2b1c3a675dbbfcc1d7fdd71c2e83adc9b
Chendi Qian <1049653012@qq.com>
2023-04-18 11:02:48 +0200

    add coalesce option

--- data/data_preprocess.py
+++ data/data_preprocess.py
@@ -100,6 +100,35 @@ class GraphToUndirected(GraphModification):
         return new_data
 
 
+class GraphCoalesce(GraphModification):
+    """
+    Wrapper of to_undirected:
+    https://pytorch-geometric.readthedocs.io/en/latest/modules/utils.html?highlight=undirected#torch_geometric.utils.to_undirected
+    """
+
+    def __call__(self, graph: Data):
+        if graph.edge_attr is not None:
+            edge_index, edge_attr = coalesce(graph.edge_index,
+                                             graph.edge_attr,
+                                             graph.num_nodes)
+        else:
+            edge_index = coalesce(graph.edge_index,
+                                  num_nodes=graph.num_nodes)
+            edge_attr = None
+
+        new_data = Data(x=graph.x,
+                        edge_index=edge_index,
+                        edge_attr=edge_attr,
+                        y=graph.y,
+                        num_nodes=graph.num_nodes)
+
+        for k, v in graph:
+            if k not in ['x', 'edge_index', 'edge_attr', 'pos', 'num_nodes', 'batch',
+                         'z', 'rd', 'node_type']:
+                new_data[k] = v
+        return new_data
+
+
 class AugmentwithNNodes(GraphModification):
 
     def __call__(self, graph: Data):
