b4824d2348b2866342a79111c6173b994b4c85b5
Chendi Qian <1049653012@qq.com>
2023-05-09 15:21:44 +0200

    unify halftransformer

--- models/downstream_models/gin_halftransformer.py
+++ models/downstream_models/gin_halftransformer.py
@@ -1,11 +1,11 @@
 import torch
-from torch_geometric.nn import Set2Set
+from torch_geometric.nn import global_mean_pool, global_add_pool, Set2Set
 
 from models.nn_modules import MLP, TransformerLayer
 from models.my_convs import BaseGIN
 
 
-class AL_HalfTransformer(torch.nn.Module):
+class GIN_HalfTransformer(torch.nn.Module):
     def __init__(self,
                  encoder,
                  head,
@@ -14,14 +14,15 @@ class AL_HalfTransformer(torch.nn.Module):
                  tf_layers,
                  hidden,
                  tf_hidden,
-                 dropout,
+                 tf_dropout,
                  attn_dropout,
                  num_classes,
                  mlp_layers_intragraph,
                  layer_norm,
                  batch_norm,
-                 use_spectral_norm):
-        super(AL_HalfTransformer, self).__init__()
+                 use_spectral_norm,
+                 graph_pooling='mean'):
+        super(GIN_HalfTransformer, self).__init__()
 
         self.encoder = encoder
 
@@ -30,17 +31,25 @@ class AL_HalfTransformer(torch.nn.Module):
             self.tf_layers.append(TransformerLayer(tf_hidden,
                                                    head,
                                                    'relu',
-                                                   dropout,
+                                                   tf_dropout,
                                                    attn_dropout,
                                                    layer_norm,
                                                    batch_norm,
                                                    use_spectral_norm))
 
-        self.gnn = BaseGIN(gnn_in_features, num_layers, hidden)
+        self.gnn = BaseGIN(gnn_in_features, num_layers, hidden, hidden, True, 0., True)
 
         # intra-graph pooling
-        self.pool = Set2Set(hidden, processing_steps=6)
-        self.mlp = MLP([2 * hidden] * mlp_layers_intragraph + [num_classes], dropout=0.)
+        if graph_pooling == "sum":
+            self.pool = global_add_pool
+        elif graph_pooling == "mean":
+            self.pool = global_mean_pool
+        elif graph_pooling == 'set2set':
+            self.pool = Set2Set(hidden, processing_steps=6)
+        else:
+            raise NotImplementedError
+
+        self.mlp = MLP([hidden if graph_pooling != 'set2set' else hidden * 2] * mlp_layers_intragraph + [num_classes], dropout=0.)
 
     def forward(self, data):
         h_node = self.encoder(data)
