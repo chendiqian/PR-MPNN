a7a8b739837b41712d6358434514899d83ff905a
Chendi Qian <1049653012@qq.com>
2023-05-11 12:16:12 +0200

    change attr name

--- training/imle_scheme.py
+++ training/imle_scheme.py
@@ -9,7 +9,7 @@ LARGE_NUMBER = 1.e10
 class IMLEScheme:
     def __init__(self, imle_sample_policy, sample_k):
         self.imle_sample_policy = imle_sample_policy
-        self.sample_k = sample_k
+        self.k = sample_k
         self.adj = None  # for potential usage
 
     @torch.no_grad()
@@ -17,16 +17,16 @@ class IMLEScheme:
 
         local_logits = logits.detach()
         if self.imle_sample_policy == 'global_topk_directed':
-            mask = rewire_global_directed(local_logits, self.sample_k)
+            mask = rewire_global_directed(local_logits, self.k)
         elif self.imle_sample_policy == 'global_topk_undirected':
             # make symmetric
             local_logits = local_logits + local_logits.transpose(1, 2)
-            mask = rewire_global_undirected(local_logits, self.sample_k)
+            mask = rewire_global_undirected(local_logits, self.k)
         elif self.imle_sample_policy == 'global_topk_semi':
             local_logits = local_logits + local_logits.transpose(1, 2)
-            mask = rewire_global_semi(local_logits, self.sample_k, self.adj)
+            mask = rewire_global_semi(local_logits, self.k, self.adj)
         elif self.imle_sample_policy == 'edge_candid':
-            mask = select_from_edge_candidates(local_logits, self.sample_k)
+            mask = select_from_edge_candidates(local_logits, self.k)
         else:
             raise NotImplementedError
 
