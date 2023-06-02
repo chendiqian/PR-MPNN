a51f6976a3e6869080531d80bdf88992fdd55eb3
Chendi Qian <1049653012@qq.com>
2023-06-02 12:00:19 +0200

    not inplace

--- training/deterministic_scheme.py
+++ training/deterministic_scheme.py
@@ -7,7 +7,7 @@ def rewire_global_directed(scores: torch.Tensor, k: int, adj: torch.Tensor):
     Batch, Nmax, _, ensemble = scores.shape
 
     k = min(k, Nmax ** 2 - torch.unique(adj[0], return_counts=True)[1].max().item())
-    scores[adj] -= LARGE_NUMBER  # avoid selecting existing edges & self loops
+    scores[adj] = scores[adj] - LARGE_NUMBER  # avoid selecting existing edges & self loops
 
     local_logits = scores.reshape(Batch, -1, ensemble)
     thresh = torch.topk(local_logits, k, dim=1, largest=True, sorted=True).values[:, -1, :][:, None, :]
@@ -21,7 +21,7 @@ def rewire_global_undirected(scores: torch.Tensor, k: int, adj: torch.Tensor):
 
     k = min(k, (Nmax * (Nmax - 1)) // 2 - torch.unique(adj[0], return_counts=True)[1].max().item())
 
-    scores[adj] -= LARGE_NUMBER  # avoid selecting existing edges & self loops
+    scores[adj] = scores[adj] - LARGE_NUMBER  # avoid selecting existing edges & self loops
     scores = scores + scores.transpose(1, 2)
     triu_idx = np.triu_indices(Nmax, k=1)
     flat_local_logits = scores[:, triu_idx[0], triu_idx[1], :]
@@ -38,7 +38,6 @@ def select_from_edge_candidates(scores: torch.Tensor, k: int):
     if k >= Nmax:
         return scores.new_ones(scores.shape)
 
-    thresh = torch.topk(scores, k, dim=1, largest=True, sorted=True).values[:, -1,
-             :][:, None, :]
+    thresh = torch.topk(scores, k, dim=1, largest=True, sorted=True).values[:, -1, :][:, None, :]
     mask = (scores >= thresh).to(torch.float)
     return mask
