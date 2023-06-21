d6ddb2341f1add0a377a49ba77c353a9bdcd21e7
Chendi Qian <1049653012@qq.com>
2023-06-21 14:10:03 +0200

    add metrics

--- data/connectness_metrics.py
+++ data/connectness_metrics.py
@@ -5,8 +5,12 @@ from torch_geometric.data import Data, Batch
 from torch_geometric.utils import (
     get_laplacian,
     to_scipy_sparse_matrix,
-    is_undirected
+    is_undirected,
+    to_networkx
 )
+import networkx as nx
+from GraphRicciCurvature.FormanRicci import FormanRicci
+from GraphRicciCurvature.OllivierRicci import OllivierRicci
 
 
 def get_second_smallest_eigval(data: Data):
@@ -36,12 +40,51 @@ def get_second_smallest_eigval(data: Data):
     return np.real(eig_vals)[0]
 
 
+def get_forman_curvature(data: nx.Graph):
+    frc = FormanRicci(data)
+    frc.compute_ricci_curvature()
+    edges = list(data.edges)
+    curvatures = [frc.G[e[0]][e[1]]['formanCurvature'] for e in edges]
+    return curvatures
+
+
+def get_ollivier_curvature(data: nx.Graph):
+    orc = OllivierRicci(data, alpha=0.5, verbose="ERROR")
+    edges = list(data.edges)
+    curv_dict = orc.compute_ricci_curvature_edges(edges)
+    return list(curv_dict.values())
+
+
 def get_connectedness_metric(data: Batch, metric: str = 'eigval'):
     assert isinstance(data.edge_index, torch.Tensor)
 
     graphs = Batch.to_data_list(data)
     if metric == 'eigval':
         metrics = [get_second_smallest_eigval(g) for g in graphs]
+    elif metric.lower() == 'smallest_formancurvature':
+        metrics = []
+        for g in graphs:
+            g = to_networkx(g, to_undirected=True, remove_self_loops=True)
+            cur_metric = get_forman_curvature(g)
+            metrics.append(min(cur_metric))
+    elif metric.lower() == 'formancurvature':
+        metrics = []
+        for g in graphs:
+            g = to_networkx(g, to_undirected=True, remove_self_loops=True)
+            cur_metric = get_forman_curvature(g)
+            metrics.extend(cur_metric)
+    elif metric.lower() == 'smallest_olliviercurvature':
+        metrics = []
+        for g in graphs:
+            g = to_networkx(g, to_undirected=True, remove_self_loops=True)
+            cur_metric = get_ollivier_curvature(g)
+            metrics.append(min(cur_metric))
+    elif metric.lower() == 'olliviercurvature':
+        metrics = []
+        for g in graphs:
+            g = to_networkx(g, to_undirected=True, remove_self_loops=True)
+            cur_metric = get_ollivier_curvature(g)
+            metrics.extend(cur_metric)
     else:
         raise NotImplementedError(f"{metric} not supported")
 
