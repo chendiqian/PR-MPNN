b44799458d87117a6a24b685d91baf8ab1b3d148
Chendi Qian <1049653012@qq.com>
2023-06-23 18:30:00 +0200

    add balanced forman

--- data/connectness_metrics.py
+++ data/connectness_metrics.py
@@ -1,16 +1,83 @@
+import math
+from collections import defaultdict
+
+import networkx as nx
 import numpy as np
 import torch
+from GraphRicciCurvature.FormanRicci import FormanRicci
+from GraphRicciCurvature.OllivierRicci import OllivierRicci
+from numba import cuda
 from scipy.sparse.linalg import eigs, eigsh
 from torch_geometric.data import Data, Batch
 from torch_geometric.utils import (
     get_laplacian,
     to_scipy_sparse_matrix,
     is_undirected,
-    to_networkx
+    to_networkx,
+    remove_self_loops,
+    to_dense_adj,
 )
-import networkx as nx
-from GraphRicciCurvature.FormanRicci import FormanRicci
-from GraphRicciCurvature.OllivierRicci import OllivierRicci
+
+
+@cuda.jit(
+    "void(float32[:,:], float32[:,:], float32[:], float32[:], int32, float32[:,:])"
+)
+def _balanced_forman_curvature(A, A2, d_in, d_out, N, C):
+    i, j = cuda.grid(2)
+
+    if (i < N) and (j < N):
+        if A[i, j] == 0:
+            C[i, j] = 0
+            return
+
+        if d_in[i] > d_out[j]:
+            d_max = d_in[i]
+            d_min = d_out[j]
+        else:
+            d_max = d_out[j]
+            d_min = d_in[i]
+
+        if d_max * d_min == 0:
+            C[i, j] = 0
+            return
+
+        sharp_ij = 0
+        lambda_ij = 0
+        for k in range(N):
+            TMP = A[k, j] * (A2[i, k] - A[i, k]) * A[i, j]
+            if TMP > 0:
+                sharp_ij += 1
+                if TMP > lambda_ij:
+                    lambda_ij = TMP
+
+            TMP = A[i, k] * (A2[k, j] - A[k, j]) * A[i, j]
+            if TMP > 0:
+                sharp_ij += 1
+                if TMP > lambda_ij:
+                    lambda_ij = TMP
+
+        C[i, j] = (
+            (2 / d_max) + (2 / d_min) - 2 + (2 / d_max + 1 / d_min) * A2[i, j] * A[i, j]
+        )
+        if lambda_ij > 0:
+            C[i, j] += sharp_ij / (d_max * lambda_ij)
+
+
+def balanced_forman_curvature(A, C=None):
+    N = A.shape[0]
+    A2 = torch.matmul(A, A)
+    d_in = A.sum(axis=0)
+    d_out = A.sum(axis=1)
+    if C is None:
+        C = torch.zeros(N, N).cuda()
+
+    threadsperblock = (16, 16)
+    blockspergrid_x = math.ceil(N / threadsperblock[0])
+    blockspergrid_y = math.ceil(N / threadsperblock[1])
+    blockspergrid = (blockspergrid_x, blockspergrid_y)
+
+    _balanced_forman_curvature[blockspergrid, threadsperblock](A, A2, d_in, d_out, N, C)
+    return C
 
 
 def get_second_smallest_eigval(data: Data):
@@ -58,74 +125,39 @@ def get_ollivier_curvature(data: nx.Graph):
 def get_connectedness_metric(data: Batch, metric: str = 'eigval'):
     assert isinstance(data.edge_index, torch.Tensor)
 
-    graphs = Batch.to_data_list(data)
-    if metric == 'eigval':
-        metrics = {
-            'eigval': [],
-        }
-
-        metrics['eigval'] = [get_second_smallest_eigval(g) for g in graphs]
-        
-    elif metric.lower() == 'smallest_formancurvature':
-        metrics = {
-            'smallest_forman_curvature': [],
-        }
-        for g in graphs:
-            g = to_networkx(g, to_undirected=True, remove_self_loops=True)
-            cur_metric = get_forman_curvature(g)
-            metrics['smallest_forman_curvature'].append(min(cur_metric))
-
-    elif metric.lower() == 'formancurvature':
-        metrics = {
-            'forman_curvature': [],
-        }
-        for g in graphs:
-            g = to_networkx(g, to_undirected=True, remove_self_loops=True)
-            cur_metric = get_forman_curvature(g)
-            metrics['forman_curvature'].extend(cur_metric)
-
-    elif metric.lower() == 'smallest_olliviercurvature':
-        metrics = {
-            'smallest_ollivier_curvature': [],
-        }
-        for g in graphs:
-            g = to_networkx(g, to_undirected=True, remove_self_loops=True)
-            cur_metric = get_ollivier_curvature(g)
-            metrics['smallest_ollivier_curvature'].append(min(cur_metric))
-
-    elif metric.lower() == 'olliviercurvature':
-        metrics = {
-            'ollivier_curvature': [],
-        }
-        for g in graphs:
-            g = to_networkx(g, to_undirected=True, remove_self_loops=True)
-            cur_metric = get_ollivier_curvature(g)
-            metrics['ollivier_curvature'].extend(cur_metric)
-
-    elif metric.lower() == 'all':
-        metrics = {
-            'forman_curvature': [],
-            'smallest_forman_curvature': [],
-            'ollivier_curvature': [],
-            'smallest_ollivier_curvature': [],
-            'eigval': [],
-        }
-        for g in graphs:
-            g = to_networkx(g, to_undirected=True, remove_self_loops=True)
-            forman_curvature = get_forman_curvature(g)
-            smallest_forman_curvature = min(forman_curvature)
-            ollivier_curvature = get_ollivier_curvature(g)
-            smallest_ollivier_curvature = min(ollivier_curvature)
-
-            metrics['forman_curvature'].extend(forman_curvature)
-            metrics['ollivier_curvature'].extend(ollivier_curvature)
-            metrics['smallest_forman_curvature'].append(smallest_forman_curvature)
-            metrics['smallest_ollivier_curvature'].append(smallest_ollivier_curvature)
-        
-        metrics['eigval'] = [get_second_smallest_eigval(g) for g in graphs]
-
-
+    if metric == 'all':
+        metrics = ['forman_curvature',
+                   'ollivier_curvature',
+                   'balanced_forman',
+                   'eigval']
     else:
-        raise NotImplementedError(f"{metric} not supported")
+        metrics = [metric.lower()]
 
-    return metrics
+    metrics_dict = defaultdict(list)
+
+    graphs = Batch.to_data_list(data)
+    nx_graphs = [to_networkx(g, to_undirected=True, remove_self_loops=True) for g in graphs]
+
+    if 'eigval' in metrics:
+        metrics_dict['eigval'] = [get_second_smallest_eigval(g) for g in graphs]
+
+    if 'forman_curvature' in metrics:
+        for g in nx_graphs:
+            cur_metric = get_forman_curvature(g)
+            metrics_dict['forman_curvature'].extend(cur_metric)
+            metrics_dict['smallest_forman_curvature'].append(min(cur_metric))
+
+    if 'ollivier_curvature' in metrics:
+        for g in nx_graphs:
+            cur_metric = get_ollivier_curvature(g)
+            metrics_dict['ollivier_curvature'].extend(cur_metric)
+            metrics_dict['smallest_ollivier_curvature'].append(min(cur_metric))
+
+    if 'balanced_forman' in metrics:
+        for g in graphs:
+            A = to_dense_adj(remove_self_loops(g.edge_index)[0])[0].cuda()  # must have cuda
+            cur_metric = balanced_forman_curvature(A).flatten().cpu().tolist()
+            metrics_dict['balanced_forman'].extend(cur_metric)
+            metrics_dict['smallest_balanced_forman'].append(min(cur_metric))
+
+    return metrics_dict
