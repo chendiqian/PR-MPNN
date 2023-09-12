72a46decc38d52fc6ba262537b3ce35cc71ef04d
Chendi Qian <1049653012@qq.com>
2023-09-12 13:11:46 +0200

    10 fold, 9:1

--- data/get_data.py
+++ data/get_data.py
@@ -464,21 +464,39 @@ def get_proteins(args):
                         transform=transform,
                         pre_transform=pre_transform)
 
+    train_splits = []
+    val_splits = []
+
+    labels = dataset.data.y.tolist()
     dataset.data.y = dataset.data.y.float()
 
-    num_training = int(len(dataset) * 0.8)
-    num_val = int(len(dataset) * 0.1)
-    num_test = len(dataset) - num_val - num_training
-    train_set, val_set, test_set = random_split(dataset,
-                                                [num_training, num_val, num_test],
-                                                generator=torch.Generator().manual_seed(0))
+    for fold_idx in range(0, 10):
+        skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=fold_idx)
+
+
+        idx_list = []
+        for idx in skf.split(np.zeros(len(labels)), labels):
+            idx_list.append(idx)
+        train_idx, test_idx = idx_list[fold_idx]
+
+        train_splits.append(Subset(dataset, train_idx))
+        val_splits.append(Subset(dataset, test_idx))
+
+    ## one split
+    # num_training = int(len(dataset) * 0.8)
+    # num_val = int(len(dataset) * 0.1)
+    # num_test = len(dataset) - num_val - num_training
+    # train_set, val_set, test_set = random_split(dataset,
+    #                                             [num_training, num_val, num_test],
+    #                                             generator=torch.Generator().manual_seed(0))
 
     if args.debug:
-        train_set = train_set[:16]
-        val_set = val_set[:16]
-        test_set = test_set[:16]
+        train_splits = train_splits[0]
+        val_splits = val_splits[0]
 
-    return train_set, val_set, test_set, None
+    test_splits = val_splits
+
+    return train_splits, val_splits, test_splits, None
 
 
 def get_treedataset(args: Union[Namespace, ConfigDict]):
